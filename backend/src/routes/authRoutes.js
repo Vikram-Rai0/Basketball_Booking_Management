293e4b515b5b4846483ec00565b8c2e92d39cbae
Vikram-Rai0 <177453319+Vikram-Rai0@users.noreply.github.com>
2026-02-15 23:11:00 +0545

    authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login ,logout,me} from '../controllers/authController.js';
+import { register, login, logout, me } from '../controllers/authController.js';
 import { authMiddleware } from '../middleware/auth.js';
 const router = express.Router();
 
